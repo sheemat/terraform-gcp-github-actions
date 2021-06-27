f1759fdf7819514c3e9f8f79a3ed9af8606a53d5
sheemat <48071652+sheemat@users.noreply.github.com>
2021-06-26 19:34:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123-mr"
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
+resource "google_storage_bucket" "gcs_bucket2" {
   name = "test-bucket-random-001124-mr"
 }
